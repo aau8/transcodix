b0b5baead436ea83131c5cfe3d6dab0698260398
Artem <artiom.ygryumov@gmail.com>
2022-07-16 12:40:08 +0300

    страница регистрации и входа

--- src/js/fix-header.js
+++ src/js/fix-header.js
@@ -1,59 +1,62 @@
 const headerArea = document.querySelector('.header-area')
-const header = headerArea.querySelector('.header')
-const main = document.querySelector('.main')
-let elY = 0;
-let scrollY = 0;
 
-headerArea.style.height = header.clientHeight + 'px'
+if (headerArea) {
+	const header = headerArea.querySelector('.header')
+	const main = document.querySelector('.main')
+	let elY = 0;
+	let scrollY = 0;
 
-fixedMenu()
-window.addEventListener('scroll', fixedMenu)
+	headerArea.style.height = header.clientHeight + 'px'
 
-function fixedMenu() {
-    const el = document.querySelector('.header');
-    const height = el.offsetHeight;
-    const pos = window.pageYOffset;
-    const diff = scrollY - pos;
+	fixedMenu()
+	window.addEventListener('scroll', fixedMenu)
 
-    elY = Math.min(0, Math.max(-height, elY + diff));
+	function fixedMenu() {
+		const el = document.querySelector('.header');
+		const height = el.offsetHeight;
+		const pos = window.pageYOffset;
+		const diff = scrollY - pos;
 
-	if (elY < 0) {
-		header.classList.remove('is-show')
-	}
-	else {
-		header.classList.add('is-show')
-	}
+		elY = Math.min(0, Math.max(-height, elY + diff));
 
-    scrollY = pos;
-
-	if (window.scrollY > 200) {
-		if (!header.classList.contains('is-fixed')) {
-			header.classList.add('is-fixed')
-			header.classList.remove('is-blur')
-			header.classList.add('is-fill')
-
-			header.style.top = -header.clientHeight + 'px'
-		}
-	}
-	else if (window.scrollY === 0) {
-
-		if (header.classList.contains('is-fixed')) {
+		if (elY < 0) {
 			header.classList.remove('is-show')
-			header.classList.remove('is-fixed')
-			header.classList.remove('is-fill')
-			header.style.top = 0
-		}
-	}
-
-	if (main) {
-
-		if (window.scrollY + header.clientHeight <= main.clientHeight) {
-			header.classList.add('is-blur')
-			header.classList.remove('is-fill')
 		}
 		else {
-			header.classList.add('is-fill')
-			header.classList.remove('is-blur')
+			header.classList.add('is-show')
+		}
+
+		scrollY = pos;
+
+		if (window.scrollY > 200) {
+			if (!header.classList.contains('is-fixed')) {
+				header.classList.add('is-fixed')
+				header.classList.remove('is-blur')
+				header.classList.add('is-fill')
+
+				header.style.top = -header.clientHeight + 'px'
+			}
+		}
+		else if (window.scrollY === 0) {
+
+			if (header.classList.contains('is-fixed')) {
+				header.classList.remove('is-show')
+				header.classList.remove('is-fixed')
+				header.classList.remove('is-fill')
+				header.style.top = 0
+			}
+		}
+
+		if (main) {
+
+			if (window.scrollY + header.clientHeight <= main.clientHeight) {
+				header.classList.add('is-blur')
+				header.classList.remove('is-fill')
+			}
+			else {
+				header.classList.add('is-fill')
+				header.classList.remove('is-blur')
+			}
 		}
 	}
 }
